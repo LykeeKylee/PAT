31236eb0418bdaeaedeb3c95100026647eb1cf32
LykeeKylee <zzh981211@gmail.com>
2020-03-15 22:40:34 +0800

    update

--- A/1010.cpp
+++ A/1010.cpp
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
-typedef unsigned long long ull;
+typedef long long ll;
 using namespace std;
 
-ull convert(string &a, ull radix) {
-    ull x = 0, coef;
+ll convert(string &a, ll radix) {
+    ll x = 0, coef;
     coef = 0;
     for(auto c = a.rbegin(); c != a.rend(); ++c) {
         x += ((*c <= '9' ? *c - '0' : *c - 'a' + 10) * pow(radix, coef));
-        coef += 1;
+        coef++;
     }
     return x;
 }
 
 int main() {
     string a, b;
-    ull tag, radix, x = 0, y = 0, coef;
+    ll tag, radix, x = 0, y = -1;
     cin >> a >> b >> tag >> radix;
     if(tag == 2) swap(a, b);
     x = convert(a, radix);
 
     char maxChar = *max_element(b.begin(), b.end());
-    int low = (maxChar <= '9' ? maxChar - '0' : maxChar - 'a' + 10) + 1;
-
+    //use isdigit(x) to judge whether x is digital or not.
+    ll low = (maxChar <= '9' ? maxChar - '0' : maxChar - 'a' + 10) + 1;
+    // note that num may be out of the range of int and long long. Avoid to use unsigned types.
+    ll high = max(low, x);
+    while(low <= high) {
+        ll mid = (low + high) / 2, tmp = convert(b, mid);
+        if(tmp == x) {
+            y = tmp;
+            radix = mid;
+            break;
+        }
+        else if(tmp > x || tmp < 0) high = mid - 1;
+        else low = mid + 1;
+    }
+    if(y == -1) cout << "Impossible";
+    else cout << radix;
     return 0;
 }
--- Readme.md
+++ Readme.md
@@ -3,23 +3,26 @@
 ## Nead to learn
 * Large integer operation
 
-## Pace
+## Paces
 * Basic Level: 95/95
-* Advanced Level: 8/155
+* Advanced Level: 10/155
 
-## Classification of problems 
+## Classifications of problems 
 * String: 1001, 1005
 * Simulation: 1002, 1006, 1008, 1009
 * Dijkstra: 1003
 * DFS&BFS: 1004
 * Dynamic Programming: 1007
 * Partition: 1007
+* Binary Search: 1010
 
 ## Some notes
 * Use fill() to initialize arrays.
 * Use #include<bits/stdc++.h> to reduce codes.
 * Main codes about DFS&BFS can be seen in problem 1004.
 * Use minmax_element, min_element, max_element, minmax, min, max to find the minimum and maximum element
+* Use isdigit(x) to judge whether x is a number or not.
+* Note that numbers may be out of the range of int and long long. Avoid to use unsigned types.It's useless.(1010)
 
   
   
